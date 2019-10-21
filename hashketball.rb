8e49950dd9652b131f3cf1b6e6bf0bfe7f7bf686
Sara Tarnvik <stvik2@gmail.com>
2019-10-21 20:14:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -127,7 +127,7 @@ end
 def shoe_size(name)
   game_hash.each do |key, value|
     value[:players].each do |player|
-      if player[:player_name] === name
+      if player[:player_name] == name
         return player[:shoe_size]
       end
     end
