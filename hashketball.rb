c48c0702a5a6151ed4dc45d00d2da5188c87bed3
Sara Tarnvik <stvik2@gmail.com>
2019-10-21 16:03:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -123,7 +123,7 @@ def num_points_scored(name)
     value.each do |info, stat|
       stat[:players].each do |player, values|
         if player[:player_name] == name
-          return player[points]
+          puts player[points]
         end
       end
     end
