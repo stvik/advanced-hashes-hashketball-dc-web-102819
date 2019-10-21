94ef09971753a8268147bdf96f061153ef135ca0
Sara Tarnvik <stvik2@gmail.com>
2019-10-21 15:40:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,20 @@
 # Write your code here!
-
+def game_hash()
+  hash = {
+    :home => {
+      :team_name =>"Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players =>[
+        ]
+    },
+    :away => {
+      :team_name =>"Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
+      :players =>[]
+    }
+    }
+  hash
+end
 
 
 
