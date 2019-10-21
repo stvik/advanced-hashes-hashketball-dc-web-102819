31092c8434f836f4cd147bbe40f965cd1efc8f5a
Sara Tarnvik <stvik2@gmail.com>
2019-10-21 15:43:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,6 +5,16 @@ def game_hash()
       :team_name =>"Brooklyn Nets",
       :colors => ["Black", "White"],
       :players =>[
+        {player_name:"Alan Anderson",
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds:12,
+          assits: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1
+        },
         ]
     },
     :away => {
