3113ce0a8c6c2f5751d182a9ff098ea8de33e468
Sara Tarnvik <stvik2@gmail.com>
2019-10-21 15:47:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -15,6 +15,46 @@ def game_hash()
           blocks: 1,
           slam_dunks: 1
         },
+        {player_name:"Reggie Evans",
+          number: 30,
+          shoe: 14,
+          points: 12,
+          rebounds:12,
+          assits: 12,
+          steals: 12,
+          blocks: 12,
+          slam_dunks: 7
+        },
+          {player_name:"Brook Lopez",
+          number: 11,
+          shoe: 17,
+          points: 17,
+          rebounds:19,
+          assits: 10,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 15
+        },
+          {player_name:"Mason Plumlee",
+          number: 1,
+          shoe: 19,
+          points: 26,
+          rebounds:11,
+          assits: 6,
+          steals: 3,
+          blocks: 8,
+          slam_dunks: 5
+        },
+          {player_name:"Jason Terry",
+          number: 31,
+          shoe: 15,
+          points: 19,
+          rebounds:2,
+          assits: 2,
+          steals: 4,
+          blocks: 11,
+          slam_dunks: 1
+        }
         ]
     },
     :away => {
